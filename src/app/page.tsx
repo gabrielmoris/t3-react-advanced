61a2eabd9435b4620e712ffbe903dfa5961f1dd6
gabriel <gabrielcmoris@gmail.com>
2025-11-15 18:30:54 +0100

    refactor HomePage component to separate image rendering logic into Images function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (images, { desc }) => [desc(images.id)],
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} alt={`Image ${image.id}`} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} alt={`Image ${image.id}`} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
