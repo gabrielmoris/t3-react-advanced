b614b8c45753a8a01d99755bc74e67b6c15d01db
gabriel <gabrielcmoris@gmail.com>
2025-11-25 21:42:48 +0100

    posthog implementation

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "../utils/uploadthing";
 import { toast } from "sonner";
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -70,8 +71,11 @@ function LoadingSpinner() {
 
 export function SimpleUploadButton() {
   const router = useRouter();
+  const posthog = usePostHog();
+
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
+      posthog.capture("upload_begin");
       toast(
         <div className="flex flex-row items-center gap-2">
           <LoadingSpinner />
